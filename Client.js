38a8323fbded6909f4b8a7d2b5ac3de6c0679025
Kariyampalli <47773037+Kariyampalli@users.noreply.github.com>
2019-06-08 09:56:29 +0200

    Add files via upload
    Client ist fertig und die anderen Funktionalitäten des Projekts
    Dokumentation ist auch fertig

--- Client.js
+++ Client.js
@@ -1,3 +1,4 @@
+/*
 const request = require ("request");
 
 
@@ -11,3 +12,77 @@ request({url, json:true}, (error,response,body) => {
  
   console.log(message);
 });
+*/
+
+
+
+
+const request = require ("request");
+var unirest = require('unirest');
+
+const addto = process.argv[2];
+const url = "http://localhost:3000/pokemonsearch_start/?pokemon=" + addto;
+
+//Christy Teil ANFANG
+request({url, json:true}, (error,response,body) => {
+  console.error('error:', error); 
+
+  var pokestring = JSON.stringify(body)
+
+  var anz = 0;
+  var newstring = "";
+  for(var i = 0; i <= pokestring.length; i++)
+  {
+    if(pokestring[i] == "<" || pokestring[i] == ">")
+    {
+      anz++;
+    }
+
+    if(anz % 2 == 0)
+    {
+      newstring = newstring + pokestring[i];
+    }
+  }
+  console.log(newstring + "\n");
+
+  var newstring2= "";
+  
+  for(var i = 30; i <= newstring.length; i++)
+  {
+    if(newstring[i] != '>' && newstring[i] != '&')
+    {
+      newstring2 = newstring2 + newstring[i];
+    } 
+   
+  }
+  
+console.log(newstring2);
+
+var newstring3= JSON.stringify(newstring2.split("\\quot;"));
+
+console.log(newstring3);
+  //var array  = newstring3.split("\\quot");
+
+
+  //var array3 = JSON.stringify(array).split(",");
+  //console.log(JSON.stringify(array3));
+
+});
+//Christy Teil ENDE -- Habs versucht Professor
+
+
+
+//Mehmets Teil ANFANG
+unirest.get("https://pokemon-go1.p.rapidapi.com/pokemon_stats.json")
+    .header("X-RapidAPI-Host", "pokemon-go1.p.rapidapi.com")
+    .header("X-RapidAPI-Key", "0569c82951mshc69f73186e5e0b3p1adc51jsnf1fc34fffc7a")
+    .end(function (result) {
+      for (var i = 0; result.body.Length; i++) {
+        let pokemon = result.body[i];
+        console.log(result.body[i].basic_attack);
+        
+        
+        var jstats=JSON.parse('{ "base_attack":result.body[i].base_attack, "base_defense":result.body[i].base_defense, "base_stamina":result.body[i].base_stamina}');
+      }
+    });
+//Mehmet Teil ENDE
